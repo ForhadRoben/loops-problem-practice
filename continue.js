6abd8e12fdeec96acc7e35b2b139b6831cc9d2da
ForhadRobe <i.rforhad@gmail.com>
2022-08-02 15:33:51 +0600

    first commit

---
+++ continue.js
@@ -0,0 +1,18 @@
+/* var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    if(number > 50){
+        continue;
+    }
+    console.log(number);
+}
+ 
+/* var marks = [13, 15, 14, 20, 18];
+for (var i = 0; i < marks.length; i++) {
+    if (marks[i] >= 15) {
+        // break;
+        continue;
+    }
+    console.log(marks[i]);
+}; */
