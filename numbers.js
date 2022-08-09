7dcd548b7788fc1be9e063801e4b3ff136ca3f00
ForhadRobe <i.rforhad@gmail.com>
2022-08-09 13:31:59 +0600

    number commited

--- numbers.js
+++ numbers.js
@@ -1,6 +1,12 @@
 // Target: Display 1 to 10
 var number = 1;
-while(number <= 20){
+while (number <= 10) {
+    console.log(number);
+    number++;
+}
+// Target: Display 1 to 20
+var number = 1;
+while (number <= 20) {
     console.log(number);
     number++;
 }
